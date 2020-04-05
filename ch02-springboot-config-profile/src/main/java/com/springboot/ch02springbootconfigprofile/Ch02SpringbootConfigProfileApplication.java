fa350fe18230af2eaa96a139ead22df23bbe56c0
Vivienne <annabel_chen@126.com>
2020-04-05 16:42:41 +0800

    ch01 springboot congfiguration profile

---
+++ ch02-springboot-config-profile/src/main/java/com/springboot/ch02springbootconfigprofile/Ch02SpringbootConfigProfileApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.ch02springbootconfigprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch02SpringbootConfigProfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch02SpringbootConfigProfileApplication.class, args);
+    }
+
+}
---
+++ ch02-springboot-config-profile/src/test/java/com/springboot/ch02springbootconfigprofile/Ch02SpringbootConfigProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.ch02springbootconfigprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch02SpringbootConfigProfileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

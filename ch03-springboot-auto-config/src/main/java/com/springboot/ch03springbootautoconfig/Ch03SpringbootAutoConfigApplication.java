7c3bd0d04cde8f690a0e3353c30ed641e2478539
Vivienne <annabel_chen@126.com>
2020-04-06 22:32:31 +0800

    ch03 springboot auto config

---
+++ ch03-springboot-auto-config/src/main/java/com/springboot/ch03springbootautoconfig/Ch03SpringbootAutoConfigApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.ch03springbootautoconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch03SpringbootAutoConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch03SpringbootAutoConfigApplication.class, args);
+    }
+
+}
---
+++ ch03-springboot-auto-config/src/test/java/com/springboot/ch03springbootautoconfig/Ch03SpringbootAutoConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.ch03springbootautoconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch03SpringbootAutoConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

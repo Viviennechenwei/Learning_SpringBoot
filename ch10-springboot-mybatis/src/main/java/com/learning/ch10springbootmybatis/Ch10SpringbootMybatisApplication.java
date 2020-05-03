9d111d4881332c407d6b3e6fe8f0f183f0b5d42b
Vivienne <annabel_chen@126.com>
2020-05-03 11:28:58 +0800

    ch10 springboot mybatis start

---
+++ ch10-springboot-mybatis/src/main/java/com/learning/ch10springbootmybatis/Ch10SpringbootMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.learning.ch10springbootmybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch10SpringbootMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch10SpringbootMybatisApplication.class, args);
+    }
+
+}
---
+++ ch10-springboot-mybatis/src/test/java/com/learning/ch10springbootmybatis/Ch10SpringbootMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.ch10springbootmybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch10SpringbootMybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

ec2f47f6411a928216189e5b25744be6c38d75c6
Vivienne <annabel_chen@126.com>
2020-05-10 22:14:37 +0800

    ch12/3 spring data jpa and jpa

---
+++ ch13-springboot-jpa/src/main/java/com/learning/ch13springbootjpa/Ch13SpringbootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.learning.ch13springbootjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch13SpringbootJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch13SpringbootJpaApplication.class, args);
+    }
+
+}
---
+++ ch13-springboot-jpa/src/test/java/com/learning/ch13springbootjpa/Ch13SpringbootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.ch13springbootjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch13SpringbootJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
